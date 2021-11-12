486136244de90e58a953c17568f68cf2dd864864
Nikita <example@gmail.com>
2021-11-12 22:11:11 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,73 @@
+'use strict'; // мы работаем в современном режиме, строгий режим
+
+
+
+
+
+
+//git
+//git init
+//git config --local user.name "Nikita"
+//git config --global user.email kudryavtsev.n.s@mail.com
+//git add -A 
+//git status
+//git commit -a -m"first commit"
+
+// Оператор !
+// const isChecked = false,
+// 	isClose = false;
+
+// console.log(isChecked || !isClose);
+
+
+// Оператор ||
+//const isChecked = true,
+// 	isClose = false;
+
+// console.log(isChecked || isClose);
+
+
+// Оператор &&
+// const isChecked = true,
+// 	isClose = false;
+
+//  console.log(isChecked && isClose);
+  
+
+// Интерполяция
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = 'Ivan';
+// alert(`Привет, ${user}`)
+
+// alert('')
+
+// const result = confirm('Are u here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18')
+// console.log(typeof(answer)); // выдает строку
+
+// const answer = +prompt('Вам есть 18?', '18')
+// console.log(typeof(answer)); // выдает number
+
+// Задание данных в массив
+// const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', '');
+// answers[1] = prompt('Какая Ваша фамилия?', '');
+// answers[2] = prompt('Сколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+//Все что пишет пользователь передается как строка
+
+//null чего-то не существует
+//undefined что-то существует но не имеет значения
+
+/* Типы данных JS
+-Простые / num str bool null undef bigint
+-Объектные / Массивы функции ОбъектДаты Рег Выражения Ошибки
+*/
