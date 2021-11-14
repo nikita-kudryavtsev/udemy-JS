eed47a8238f19d36bf330688367a9e8c61d1f722
Nikita <example@gmail.com>
2021-11-14 06:36:22 +0500

    14lesson

--- js/script.js
+++ js/script.js
@@ -3,6 +3,33 @@
 
 
 
+//УРОК 14 ЦИКЛЫ
+// let num = 50;
+
+// while (num <= 55) {
+//   console.log(num);
+//   num++;
+// }
+
+// do {
+//   console.log(num);
+//   num++;
+// }
+// while (num < 55);
+
+// for (let i = 1; i < 8; i++) {
+//   console.log(num);
+//   num++;
+// }
+
+// for (let i = 1; i < 10; i++) {
+// 	if (i === 6) {
+//     //break;
+//     continue;
+//   }
+//   console.log(i);
+// }
+
 
 
 // УРОК 13 УСЛОВИЯ 
