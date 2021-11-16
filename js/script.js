f008c3697d38ea61954fd1389e24c0b27400b65c
Nikita <example@gmail.com>
2021-11-17 01:05:12 +0500

    18pract

--- js/script.js
+++ js/script.js
@@ -1,43 +1,117 @@
 'use strict'; // мы работаем в современном режиме, строгий режим
 
-//Методы и свойства у строк и чисел
+//УРОК 18 ПРАКТИКА ИСПОЛЬЗОВАНИЯ ФУНКЦИЙ
+
+let numberOfFilms;
+
+function start() {
+	numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+	while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+		numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+	}
+}
+
+start();
+
+const personalMovieDB = {
+	count: numberOfFilms,
+	movies: {},
+	actors: {},
+	genres: [],
+	privat: false,
+};
+
+
+// function writeYourGenres() {
+//   const a = prompt(`Ваш любимый жанр под номером ${}`)
+//   const b = prompt(`Ваш любимый жанр под номером ${}`)
+//   const c = prompt(`Ваш любимый жанр под номером ${}`)
+// }
+
+// personalMovieDB.genres = [a, b, c, d]
+
+function rememberMyFilms() {
+	for (let i = 0; i < 1; i++) {
+		const a = prompt('Один из просмотренных фильмов?', ''),
+			    b = prompt('На сколько оцените его?', '');
+
+		if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+			personalMovieDB.movies[a] = b;
+			console.log('done');
+		} else {
+			console.log('error');
+			i--;
+		}
+	}
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+	if (personalMovieDB.count < 10) {
+		console.log('Просмотренно довольно мало фильмов!');
+	} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+		console.log('Вы классический зритель!');
+	} else if (personalMovieDB.count >= 30) {
+		console.log('Вы киноман!');
+	} else {
+		console.log('Произошла ошибка');
+	}
+}
+
+detectPersonalLevel();
+
+function showMyDB(hidden) {
+	if (!hidden) {
+		console.log(personalMovieDB);
+	}
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++) { 
+    personalMovieDB.genres[i - 1] =  prompt(`Ваш любимый жанр под номером ${i}`);
+  }
+}
+writeYourGenres()
+//17 УРОК Методы и свойства у строк и чисел
 //Строки
-const str = 'test';
-const arr = [1, 2, 3]
+// const str = 'test';
+// const arr = [1, 2, 3]
 
-// console.log(str[2]);
-console.log(arr.length); //свойство
+// // console.log(str[2]);
+// console.log(arr.length); //свойство
 
-console.log(str.toUpperCase()); // метод
-console.log(str.toLowerCase());
+// console.log(str.toUpperCase()); // метод
+// console.log(str.toLowerCase());
 
-const fruit = 'Some fruit'
-console.log(fruit.indexOf('fruit'));
+// const fruit = 'Some fruit'
+// console.log(fruit.indexOf('fruit'));
 
+// const logg = 'hello world';
 
-const logg = 'hello world';
+// console.log(logg.slice(6, 11)); //Поддерживает минусы
 
-console.log(logg.slice(6, 11)); //Поддерживает минусы
+// console.log(logg.substring(6, 11)); //Не поддерживает
+// console.log(logg.substr(6, 5)); //Не поддерживает
 
-console.log(logg.substring(6, 11)); //Не поддерживает
-console.log(logg.substr(6, 5)); //Не поддерживает
+// //Числа
+// const num = 12.2;
+// console.log(Math.round(num)); //Округление
 
-//Числа
-const num = 12.2;
-console.log(Math.round(num)); //Округление
-
-const test = '12.2px'
-//console.log(parseInt(test)); //Переводит число в другую систему исчисления
-console.log(parseFloat(test)); //Возвращает с десятичными
-
+// const test = '12.2px'
+// //console.log(parseInt(test)); //Переводит число в другую систему исчисления
+// console.log(parseFloat(test)); //Возвращает с десятичными
 
 // УРОК 16 Стрелочные функции
-// let num = 20; 
+// let num = 20;
 
 // Function Declaration - существует до ее объявления
 // function showFirstMessage(text) {
-//   console.log(text);  
-//   let num = 10; 
+//   console.log(text);
+//   let num = 10;
 //   console.log(num);
 // }
 
@@ -51,7 +125,6 @@ console.log(parseFloat(test)); //Возвращает с десятичными
 // console.log(calc(5, 6));
 // console.log(calc(10, 6));
 
-
 // function ret() {
 //   let num = 50;
 //   return num;
@@ -60,19 +133,17 @@ console.log(parseFloat(test)); //Возвращает с десятичными
 // console.log(anotherNum);
 
 // //Function expression - создается только после объявления
-// // 
+// //
 // const logger = function() {
 //   console.log('hello');
 // };
 // logger();
 // // Стрелочные функции
 
-// const calc = (a, b) => { 
+// const calc = (a, b) => {
 //   console.log('1');
-//   return a + b 
+//   return a + b
 // };
-
-
 
 // Урок 15 Практика Циклы
 // const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
@@ -110,8 +181,6 @@ console.log(parseFloat(test)); //Возвращает с десятичными
 //   }
 
 // personalMovieDB.count < 10 ? console.log('Просмотренно мало фильмов'): personalMovieDB.count >=10 && personalMovieDB.count < 30 ? console.log('Классический зритель'): personalMovieDB.count >=30 ? console.log('Киноман') :console.log('Ошибка ввода');
-
-
 
 //УРОК 14 ЦИКЛЫ
 // let num = 50;
@@ -168,7 +237,6 @@ console.log(parseFloat(test)); //Возвращает с десятичными
 //     console.log('не в этот раз');
 // }
 // switch всегда идет на строгое сравнение
-
 
 // 12 Урок 1 задание
 // const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
