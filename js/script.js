f022a64662ee1e18612f1f36c5bde01268fdd06d
Nikita <example@gmail.com>
2021-11-19 17:38:20 +0500

    26th less

--- js/script.js
+++ js/script.js
@@ -1,75 +1,132 @@
 'use strict'; // мы работаем в современном режиме, строгий режим
 
+
+//УРОК 26 ДИНАМИЧЕСКАЯ ТИПИЗАЦИЯ 
+// ДТ - возможность одного типа данных превращаться в другой
+
+//To string 
+
+//1) 
+// console.log(typeof(String(null)));
+// console.log(typeof(String(4)));
+//2) console
+console.log(typeof(5 + '')); 
+
+const num = 5;
+
+console.log('https://vk.com/catalog/' + num);
+
+const fontSize = 26 + 'px';
+
+// to number
+
+//1) 
+console.log(typeof(Number('4')));
+
+//2) унарный плюс
+console.log(typeof(+'5'));
+
+//3) 
+console.log(typeof(parseInt('15px', 10)));
+
+let answ = +prompt('Hello', '');
+
+// to boolean 
+
+// 0, '', null, undefined, NaN; - false 
+
+// 1)
+let switcher = null;
+
+if (switcher) {
+	console.log('Working...');
+}
+
+switcher = 1;
+
+if (switcher) {
+	console.log('Working...');
+}
+
+// 2) 
+console.log(typeof(Boolean('4')));
+
+//3
+console.log(typeof(!!'444'));
+
+
+
+
 //УРОК 24 ПРАКТИКА, ИСПОЛЬЗУЕМ ОБЪЕКТЫ
 
-const personalMovieDB = {
-	count: 0,
-	movies: {},
-	actors: {},
-	genres: [],
-	privat: false,
-	start: function () {
-		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// const personalMovieDB = {
+// 	count: 0,
+// 	movies: {},
+// 	actors: {},
+// 	genres: [],
+// 	privat: false,
+// 	start: function () {
+// 		personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-		while (
-			personalMovieDB.count == '' ||
-			personalMovieDB.count == null ||
-			isNaN(personalMovieDB.count)
-		) {
-			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
-		}
-	},
-	rememberMyFilms: function () {
-		for (let i = 0; i < 1; i++) {
-			const a = prompt('Один из просмотренных фильмов?', ''),
-				b = prompt('На сколько оцените его?', '');
+// 		while (
+// 			personalMovieDB.count == '' ||
+// 			personalMovieDB.count == null ||
+// 			isNaN(personalMovieDB.count)
+// 		) {
+// 			personalMovieDB.count = +prompt('Сколько фильмов вы уже посмотрели?', '');
+// 		}
+// 	},
+// 	rememberMyFilms: function () {
+// 		for (let i = 0; i < 1; i++) {
+// 			const a = prompt('Один из просмотренных фильмов?', ''),
+// 				b = prompt('На сколько оцените его?', '');
 
-			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-				personalMovieDB.movies[a] = b;
-				console.log('done');
-			} else {
-				console.log('error');
-				i--;
-			}
-		}
-	},
-	detectPersonalLevel: function () {
-		if (personalMovieDB.count < 10) {
-			console.log('Просмотренно довольно мало фильмов!');
-		} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
-			console.log('Вы классический зритель!');
-		} else if (personalMovieDB.count >= 30) {
-			console.log('Вы киноман!');
-		} else {
-			console.log('Произошла ошибка');
-		}
-	},
-	showMyDB: function (hidden) {
-		if (!hidden) {
-			console.log(personalMovieDB);
-		}
-	},
-	toggleVisibleMyDB: function () {
-		if (personalMovieDB.privat) {
-			personalMovieDB.privat = false;
-		} else {
-			personalMovieDB.privat = true;
-		}
-	},
-	writeYourGenres: function () {
-		for (let i = 1; i <= 3; i++) {
-			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
-			if (genre == '' || genre === null) {
-				console.log('Вы ввели некорректные данные');
-			} else {
-				personalMovieDB.genres[i - 1] = genre;
-			}
-		}
-		personalMovieDB.genres.forEach((item, i) => {
-			console.log(`"Любимый жанр ${i + 1} - это ${item}`);
-		});
-	},
-};
+// 			if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+// 				personalMovieDB.movies[a] = b;
+// 				console.log('done');
+// 			} else {
+// 				console.log('error');
+// 				i--;
+// 			}
+// 		}
+// 	},
+// 	detectPersonalLevel: function () {
+// 		if (personalMovieDB.count < 10) {
+// 			console.log('Просмотренно довольно мало фильмов!');
+// 		} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+// 			console.log('Вы классический зритель!');
+// 		} else if (personalMovieDB.count >= 30) {
+// 			console.log('Вы киноман!');
+// 		} else {
+// 			console.log('Произошла ошибка');
+// 		}
+// 	},
+// 	showMyDB: function (hidden) {
+// 		if (!hidden) {
+// 			console.log(personalMovieDB);
+// 		}
+// 	},
+// 	toggleVisibleMyDB: function () {
+// 		if (personalMovieDB.privat) {
+// 			personalMovieDB.privat = false;
+// 		} else {
+// 			personalMovieDB.privat = true;
+// 		}
+// 	},
+// 	writeYourGenres: function () {
+// 		for (let i = 1; i <= 3; i++) {
+// 			let genre = prompt(`Ваш любимый жанр под номером ${i}`);
+// 			if (genre == '' || genre === null) {
+// 				console.log('Вы ввели некорректные данные');
+// 			} else {
+// 				personalMovieDB.genres[i - 1] = genre;
+// 			}
+// 		}
+// 		personalMovieDB.genres.forEach((item, i) => {
+// 			console.log(`"Любимый жанр ${i + 1} - это ${item}`);
+// 		});
+// 	},
+// };
 
 // УРОК 23 ОСНОВЫ ООП, прототипно-ориентированное наследие
 
